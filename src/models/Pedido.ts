e3804c08a53bd3fbccf6ea2e640ac6e1413b2274
bryanborges23 <bryanborges027@gmail.com>
2026-03-25 21:39:54 -0300

    Add optional id field to Pedido interface

--- src/models/Pedido.ts
+++ src/models/Pedido.ts
@@ -1,3 +1,10 @@
 export interface Pedido {
-    
+      id?: number;
+  id_cliente: number;
+  id_funcionario: number;
+  endereco: string;
+  tipo_pedido: string;
+  data_pedido: Date;
+  valor: number;
+  status_pedido: string;
 }
